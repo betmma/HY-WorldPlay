7c50d5e4c559efa4235fa17ec8dc87ac0fe778ff
betmma <116655315+betmma@users.noreply.github.com>
2026-05-10 11:35:09 +0800

    fix AttributeError: torch._dynamo.config.recompile_limit does not exist

--- trainer/layers/lora/linear.py
+++ trainer/layers/lora/linear.py
@@ -20,7 +20,7 @@ from trainer.layers.linear import (ColumnParallelLinear, LinearBase,
 from trainer.layers.vocab_parallel_embedding import VocabParallelEmbedding
 from trainer.utils import get_mixed_precision_state
 
-torch._dynamo.config.recompile_limit = 16
+# torch._dynamo.config.recompile_limit = 16
 
 
 class BaseLayerWithLoRA(nn.Module):
